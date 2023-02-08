bd5feb97d715ba38f134d8eb390b3128c9d25cf0
Bruno Kappi de Matos <brunokappidematos2@gmail.com>
2023-02-08 11:19:13 -0300

    Update index.js

--- src/components/firebase/index.js
+++ src/components/firebase/index.js
@@ -3,17 +3,6 @@ import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-/*
-const firebaseConfig = {
-    apiKey: "AIzaSyCKo6iAFke34en7Xqescj7yiW_xRUwlq8s",
-    authDomain: "notesapp-f28fa.firebaseapp.com",
-    projectId: "notesapp-f28fa",
-    storageBucket: "notesapp-f28fa.appspot.com",
-    messagingSenderId: "689812196245",
-    appId: "1:689812196245:web:b2c1d766d6592de849049c",
-    measurementId: "G-4M2RFX8GEV"
-  };*/
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey, 
   authDomain: process.env.REACT_APP_authDomain, 
